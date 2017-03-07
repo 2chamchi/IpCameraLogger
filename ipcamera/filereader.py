543938370be30085d85842ce0a1ab257724cb31b
leebw <fissherr@gmail.com>
2017-03-07 17:37:08 +0900

    add to filter method

--- ipcamera/filereader.py
+++ ipcamera/filereader.py
@@ -1,33 +1,30 @@
-import sys
 import codecs
-from ipcamera.ipcamera import IpCamera
 from collections import namedtuple
 
 class FileReader(object):
-
     def __init__(self, path):
-        print('__init reader__')
         self.path = path
-        print('read_camera_list', self.path)
         self.file = codecs.open(self.path, 'r', 'utf-8')
 
     def get_camera_list(self):
-
         lines = self.file.readlines()
-        camera_info_list = []
+        self.camera_info_list = []
         for idx in lines:
             camera_info = idx.split(',')
-            camera_info_list.append(self.parse_info(camera_info))
-        return camera_info_list
+            self.camera_info_list.append(self.parse_info(camera_info))
+        self.file.close()
 
     def parse_info(self, line):
         camera_structure = namedtuple("camera_structure", "name id password ip")
-        name = line[0].replace("\ufeff", "")
-        id = line[1]
-        password = line[2]
-        ip = line[3].replace("\r\n", "")
+        name = self.str_filter(line[0])
+        id = self.str_filter(line[1])
+        password = self.str_filter(line[2])
+        ip = self.str_filter(line[3])
         return camera_structure(name, id, password, ip)
 
-    def log(self, str):
-        print(sys.stderr, str)
+    def str_filter(self, line_str):
+        result = line_str.replace("\ufeff", "")
+        result = result.replace("\r", "")
+        result = result.replace("\n", "")
+        return result
 
